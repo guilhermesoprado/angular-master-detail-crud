61be77e3cab736c6f869ef612a692d7ee7fa930a
unknown <guilhermesoprado@gmail.com>
2021-03-30 18:01:33 -0300

    adding category model

--- src/app/pages/in-memory-database.ts
+++ src/app/pages/in-memory-database.ts
@@ -1,8 +1,13 @@
 import { InMemoryDbService } from 'angular-in-memory-web-api';
 
+import { Category } from './categories/shared/category.model';
+
+
+
 export class InMemoryDatabase implements InMemoryDbService{
+    
     createDb(){
-        const categories = [
+        const categories : Category[] = [
             { id: 1, name: 'Moradia', description: 'Pagamentos de Contas da Casa' },
             { id: 2, name: 'Saúde', description: 'Plano de Saúde e Remédios' },
             { id: 3, name: 'Lazer', description: 'Cinema, parques, praia, etc' },
@@ -12,4 +17,5 @@ export class InMemoryDatabase implements InMemoryDbService{
 
         return { categories };
     }
+
 }
