6aefdf5f96e3ae5ff3d76ba51849bbcdf7a2e4c7
unknown <guilhermesoprado@gmail.com>
2021-03-30 18:46:31 -0300

    adding category service

---
+++ src/app/pages/categories/shared/category.service.ts
@@ -0,0 +1,66 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { Category } from './category.model';
+import { map, catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+
+  private apiPath = "api/categories";
+  constructor(private http:HttpClient) { }
+
+  getAll(): Observable<Category[]>{
+    return this.http.get<Category[]>(this.apiPath).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataToCategories)
+    )
+  }
+
+  getById(id: number):Observable<Category>{
+    const url = `${this.apiPath}/${id}`;
+    return this.http.get<Category>(url).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataToCategory)
+    )
+  }
+
+  create(category: Category): Observable<Category>{
+    return this.http.post<Category>("api/category",category).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataToCategory)
+    );
+  }
+
+  update(category: Category): Observable<Category>{
+    const url = `${this.apiPath}/${category.id}`;
+    return this.http.put<Category>(url,category).pipe(
+      catchError(this.handleError),
+      map(() => category)
+    );
+  }
+
+  delete(id: number): Observable<any>{
+    const url = `${this.apiPath}/${id}`;
+    return this.http.delete<any>(url).pipe(
+      catchError(this.handleError),
+      map(() => null)
+    );
+  }
+
+  private jsonDataToCategories(jsonData:any[]): Category[]{
+    const categories: Category[] = [];
+    jsonData.forEach(element => categories.push(element as Category));
+    return categories;
+  }
+  private jsonDataToCategory(jsonData:any): Category{
+    return jsonData as Category;
+  }
+  
+  private handleError(error: any): Observable<any>{
+    console.log("Error request => ", error );
+    return throwError(error);
+  }
+}
