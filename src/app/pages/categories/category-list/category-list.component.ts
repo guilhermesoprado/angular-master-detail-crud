756b6c1cbd70bbb710feb98ddd19b7d96efe77ae
unknown <guilhermesoprado@gmail.com>
2021-03-31 17:50:22 -0300

    improving category form

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from '../shared/category.model';
+import { CategoryService } from '../shared/category.service';
 
 @Component({
   selector: 'app-category-list',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./category-list.component.css']
 })
 export class CategoryListComponent implements OnInit {
-
-  constructor() { }
+  public categories: Category[] = [];
+  constructor(private categoryService: CategoryService) { }
 
   ngOnInit(): void {
+    this.getAll();
+  }
+
+  getAll():void{
+    this.categoryService.getAll().subscribe(
+      //categories => console.log(categories),
+      categories =>  this.categories = categories,
+      error => alert("Error loading the category list.")
+    )
+  }
+
+  delete(category: Category):void{
+    const mustDelete = confirm("Would you like to delete this category?");
+
+    if(mustDelete)
+      this.categoryService.delete(category.id).subscribe(
+        ()=> this.categories = this.categories.filter(element => element != category),
+        () => alert("Delete failed")
+      )
   }
 
 }
